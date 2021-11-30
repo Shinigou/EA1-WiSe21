a1ee177f1eb0204118fe629f68c5ec94085bdba2
Shinigou <92162143+Shinigou@users.noreply.github.com>
2021-11-30 22:55:02 +0100

    update

--- Aufgabe_7/script.ts
+++ Aufgabe_7/script.ts
@@ -32,13 +32,13 @@ window.addEventListener ( "load", function () {
 function play() {
 setInterval(function() {
     (sound[0]).play();
-},1000);
+},500);
 
 setInterval(function() {
     (sound[1]).play();
-}, 3000);
+}, 1000);
 
 setInterval(function() {
     (sound[2]).play();
-}, 5000);
+}, 1500);
 }
